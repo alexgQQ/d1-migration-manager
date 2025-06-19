f6236e4c73e3f5f37a35eda402a9d5176a8f083e
Alex Grand <me@alexgrand.dev>
2025-06-19 10:49:34 -0600

    Harden the migration file handling utils

--- d1_migration_manager/migration.py
+++ d1_migration_manager/migration.py
@@ -1,34 +1,84 @@
 import os
+import re
 import sqlite3
 from datetime import UTC, datetime
+from glob import glob
 
 from d1_migration_manager import ChangeEvent
 
 
 class MigrationFile:
-    @staticmethod
-    def header(num, dt):
-        dt = dt.strftime("%Y-%m-%dT%H:%M:%SZ")
-        return f"-- Migration number: {num:04} \t {dt}\n"
+    """
+    Interface functionality specifically for dealing with the sql migration files used with wrangler
+    A migration directory should only contain sql files created and handled through this interface
+    The files must be named starting with it's order number padded to 4 places and a snake case formatted message
+        example:    `0001_initial_migration.sql`
+    Each file must have a SQL comment as the first line that contains the migration number and it's creation date
+        example:    `-- Migration number: 0001 	 2025-04-02T15:08:54.407Z`
+    These are important to keep consistent as the migration order is determined from the filename and the creation
+    dates determine what time frame to check for changes
+    """
 
     @staticmethod
-    def parse_header(header):
+    def header(num: int, dt: datetime) -> str:
+        """Create a valid migration file header"""
+        if dt.tzinfo is None:
+            raise ValueError("datetime must be timezone aware")
+        elif dt.tzinfo is not UTC:
+            dt = dt.astimezone(UTC)
+
+        # Get the specific str datetime format used above
+        # 3 decimal millisecond and Z for UTC as datetime module does not support it in isoformat
+        dt = dt.isoformat(timespec="milliseconds").replace("+00:00", "Z")
+        return f"-- Migration number: {num:04} \t {dt}"
+
+    @staticmethod
+    def parse_header(header: str) -> tuple[int, datetime]:
+        """Get the migration number and datetime from migration file header"""
         header = header.split("\t")
         if len(header) != 2:
-            return False
-        mig_num, mig_date = header
+            raise ValueError(f"header malformed - {header}")
+        num, dt = header
         try:
-            mig_num = int(mig_num.split(":")[-1])
-            mig_date = datetime.fromisoformat(mig_date.strip())
-        except Exception as err:
-            print(err)
-            return False
-        return mig_num, mig_date
+            num = num.split(":")[-1]
+            num = int(num)
+            dt = datetime.fromisoformat(dt.strip())
+        except (IndexError, ValueError) as err:
+            raise ValueError(f"unable to parse header values - {err}")
+        return num, dt
 
     @staticmethod
-    def filename(message, number):
-        slugged = message.strip().lower().replace(" ", "_")
+    def filename(message: str, number: int) -> str:
+        """Create a valid migration filename"""
+        slugged = message.lower().strip()
+        # Remove non-alphanumeric except spaces and hyphens
+        slugged = re.sub(r"[^a-z0-9\s-]", "", slugged)
+        # Condense multiple internal whitespace
+        slugged = re.sub(r"\s+", " ", slugged)
+        slugged = slugged.replace(" ", "_").replace("-", "_")
         return f"{number:04}_{slugged}.sql"
+
+    @staticmethod
+    def latest(directory: str) -> str | None:
+        """Get the filepath of the latest migration file in a directory"""
+        files = glob(os.path.join(directory, "*"))
+        if any((not f.endswith(".sql") for f in files)):
+            raise RuntimeError(
+                f"migrations directory must only contain sql files - {directory}"
+            )
+        count = len(files)
+        # Match files to the expected migration filename format
+        files = [
+            f
+            for f in files
+            if re.match(r"^\d{4}_[a-z0-9].*", os.path.basename(f)) is not None
+        ]
+        if len(files) != count:
+            raise RuntimeError("unexpected migration file encountered")
+        elif len(files) <= 0:
+            return None
+        last = sorted(files)[-1]
+        return last
 
 
 # TODO: This is meant to coalesce simple python types into their valid SQL syntax
--- tests/test_migration.py
+++ tests/test_migration.py
@@ -1,6 +1,9 @@
+import os
 import sqlite3
 import unittest
 from datetime import datetime
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from d1_migration_manager import (MigrationFile, delete_sql, insert_sql,
                                   update_sql)
@@ -78,11 +81,39 @@ class TestMigrationFile(unittest.TestCase):
         filename = MigrationFile.filename("this is a test", 5)
         expected = "0005_this_is_a_test.sql"
         self.assertEqual(filename, expected)
+        filename = MigrationFile.filename(" &THIS?  @ IS-A  TEST! ", 5)
+        expected = "0005_this_is_a_test.sql"
+        self.assertEqual(filename, expected)
 
     def test_parse_header(self):
-        expected_num = 2
-        expected_dt = datetime.fromisoformat("2025-06-17T23:13:39Z")
-        header = "-- Migration number: 0002 	 2025-06-17T23:13:39Z"
+        expected_num = 1
+        expected_dt = datetime.fromisoformat("2025-04-02T15:08:54.407Z")
+        header = "-- Migration number: 0001 	 2025-04-02T15:08:54.407Z"
         num, dt = MigrationFile.parse_header(header)
         self.assertEqual(num, expected_num)
         self.assertEqual(dt, expected_dt)
+
+    def test_header(self):
+        num = 1
+        dt = datetime.fromisoformat("2025-04-02T15:08:54.407Z")
+        expected_header = "-- Migration number: 0001 	 2025-04-02T15:08:54.407Z"
+        header = MigrationFile.header(num, dt)
+        self.assertEqual(header, expected_header)
+
+    def test_latest(self):
+        first_file = "0001_initial_migration.sql"
+        second_file = "0002_update_table.sql"
+        bad_format = "notamigration.sql"
+        with TemporaryDirectory() as dirname:
+            expected = os.path.join(dirname, first_file)
+            Path(expected).touch()
+            path = MigrationFile.latest(dirname)
+            self.assertEqual(path, expected)
+            expected = os.path.join(dirname, second_file)
+            Path(expected).touch()
+            path = MigrationFile.latest(dirname)
+            self.assertEqual(path, expected)
+            bad_format = os.path.join(dirname, bad_format)
+            Path(bad_format).touch()
+            with self.assertRaises(RuntimeError):
+                MigrationFile.latest(dirname)
